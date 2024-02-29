c2aa969f6cd2707a80f9c3d06491e6c1f885a7bd
Jano <martinojano0@gmail.com>
2024-02-29 02:46:00 -0300

    update readme

--- ui/Button/ButtonOpenCart.jsx
+++ ui/Button/ButtonOpenCart.jsx
@@ -10,7 +10,15 @@ export default function ButtonOpenCart() {
     <button
       onClick={() => handleClick()}
       id="button-open-cart"
-      className="fixed top-15 right-0 mt-4 mr-4 inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+      style={{
+        position: "fixed",
+        top: "15",
+        right: "0",
+        backgroundColor: "red",
+        margin: "20px 10px 0px 0px",
+        padding: "5px",
+      }}
+      className="inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
     >
       🛒 Abrir Carrito
     </button>
